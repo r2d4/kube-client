52b33629b219870d6f94c618af0a75712ab2b8d8
r2d4 <m@rickard.email>
2017-03-14 18:24:56 -0700

    v1.6.0-beta.3

---
+++ client-go/pkg/apis/storage/install/install.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package install installs the experimental API group, making it available as
+// an option to all of the API encoding/decoding machinery.
+package install
+
+import (
+	"github.com/r2d4/kube-client/apimachinery/pkg/apimachinery/announced"
+	"github.com/r2d4/kube-client/apimachinery/pkg/apimachinery/registered"
+	"github.com/r2d4/kube-client/apimachinery/pkg/runtime"
+	"github.com/r2d4/kube-client/apimachinery/pkg/util/sets"
+	"github.com/r2d4/kube-client/client-go/pkg/api"
+	"github.com/r2d4/kube-client/client-go/pkg/apis/storage"
+	"github.com/r2d4/kube-client/client-go/pkg/apis/storage/v1"
+	"github.com/r2d4/kube-client/client-go/pkg/apis/storage/v1beta1"
+)
+
+func init() {
+	Install(api.GroupFactoryRegistry, api.Registry, api.Scheme)
+}
+
+// Install registers the API group and adds types to a scheme
+func Install(groupFactoryRegistry announced.APIGroupFactoryRegistry, registry *registered.APIRegistrationManager, scheme *runtime.Scheme) {
+	if err := announced.NewGroupMetaFactory(
+		&announced.GroupMetaFactoryArgs{
+			GroupName: storage.GroupName,
+			// TODO:  change the order when GKE supports v1
+			VersionPreferenceOrder:     []string{v1beta1.SchemeGroupVersion.Version, v1.SchemeGroupVersion.Version},
+			ImportPrefix:               "k8s.io/client-go/pkg/apis/storage",
+			RootScopedKinds:            sets.NewString("StorageClass"),
+			AddInternalObjectsToScheme: storage.AddToScheme,
+		},
+		announced.VersionToSchemeFunc{
+			v1.SchemeGroupVersion.Version:      v1.AddToScheme,
+			v1beta1.SchemeGroupVersion.Version: v1beta1.AddToScheme,
+		},
+	).Announce(groupFactoryRegistry).RegisterAndEnable(registry, scheme); err != nil {
+		panic(err)
+	}
+}
